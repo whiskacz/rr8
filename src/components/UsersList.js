6cc110798b0bc724e88feb908ee7a8bd59a0e188
whiskacz <pawelblasiak1985@gmail.com>
2023-08-26 20:56:09 +0200

    added tailwind + async loading

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,12 +1,30 @@
-import { useEffect} from 'react'
-import { useDispatch } from 'react-redux'
-import { fetchUsers } from '../store'
+import { useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
+import  Skeleton  from './Skeleton';
 
 export default function UsersList() {
   const dispatch = useDispatch();
 
+  const { data, isLoading, error } = useSelector((state) => {
+    return state.users;
+  })
+
+  
   useEffect(() => {
     dispatch(fetchUsers())
   },[dispatch]);
+  
+  if(isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />
+  }
+  if(error) {
+    return <div>ERROR</div>
+  }
+
+  if(data) {
+    return <div>{data.length}</div>
+  }
+  
   return 'Users List'
-};
+}
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,6 +4,12 @@ import axios from "axios";
 export const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users')
 
+    await pause(5000)
     return response.data
 })
 
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve,duration)
+    })
+}
